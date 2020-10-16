c1b4e9e4401f3da85ba7c1a6ada009cb78325178
Wesley <wesley.jaboin@gmail.com>
2020-10-16 08:50:29 -0400

    final project

--- src/App.js
+++ src/App.js
@@ -85,17 +85,4 @@ function App() {
     
   
     
-/* <>
- <h1> products </h1>
- <div className= "products"> 
- {products.map((product, idx) => (
-   <div className ="product" key ={idx}> 
-     <h3> {product.name} </h3>
-     <h4> {product.price} </h4>
-     <img src = {product.img} alt ={ product.name}/>
-     <button onClick ={() => addToCart(product)}> ADD TO CART</button>
-   </div>
- ))}
 
-</div>
-</>*/
